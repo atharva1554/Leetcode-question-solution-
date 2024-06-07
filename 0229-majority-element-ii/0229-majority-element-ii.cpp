a7ac0468aee34025615200817b60ee744d74dd25
Atharva Vijay Khairnar <atharva.khairnar_comp21@pccoer.in>
2024-06-07 17:59:34 +0530

    Time: 11 ms (41.89%), Space: 18.4 MB (44.95%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,33 +1,79 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        vector<int>a;
+        int el1,el2,cnt1=0,cnt2=0;
+        el1=INT_MIN;
+        el2=INT_MIN;
         int n=nums.size();
+        vector<int>a;
         for(int i=0;i<n;i++)
         {
-            int count=(n/3)+1;
-            for(int j=0;j<n;j++)
+            if(cnt1==0&&nums[i]!=el2)
             {
-                if(nums[i]==nums[j])
-                {
-                    count--;
-                }
+                cnt1=1;
+                el1=nums[i];
             }
-            if(count<=0)
-            {   int x=0;
-               for(int k=0;k<a.size();k++)
-               {
-                 if(nums[i]==a[k])
-                 {
-                    x=1;
-                 }
-               }
-               if(x==0)
-               {
-                 a.push_back(nums[i]);
-               }
+            else if(cnt2==0&&nums[i]!=el1)
+            {
+                cnt2=1;
+                el2=nums[i];
+            }
+            else if(nums[i]==el1)
+            {
+                cnt1++;
+            }
+            else if(nums[i]==el2)
+            {
+                cnt2++;
+            }
+            else 
+            {
+                cnt1--;
+                cnt2--;
+            }
+
+        }
+        cnt1=0,cnt2=0;
+        for(int i=0;i<n;i++)
+        {
+            if(el1==nums[i])
+            {
+                cnt1++;
+            }
+            if(el2==nums[i])
+            {
+                cnt2++;
             }
         }
+        int min=int (n/3)+1;
+        
+        
+            if(el1<el2)
+            {   
+                
+               
+              if(cnt1>=min)
+                {
+                     a.push_back(el1);
+                }
+                 if(cnt2>=min)
+                {
+                   a.push_back(el2);
+                }
+            }
+            else 
+            {
+                 if(cnt2>=min)
+                {
+                   a.push_back(el2);
+                }
+              if(cnt1>=min)
+                {
+                     a.push_back(el1);
+                }
+               
+            }
+        
         return a;
     }
 };
