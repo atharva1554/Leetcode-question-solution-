423c4718e60af935f54e4989033227ec248d1f54
Atharva Vijay Khairnar <atharva.khairnar_comp21@pccoer.in>
2024-06-10 19:28:57 +0530

    Time: 0 ms (100%), Space: 10.1 MB (78.64%) - LeetHub

---
+++ 1137-height-checker/1137-height-checker.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        int n=heights.size();
+        int count=0;
+        vector<int>a=heights;
+        sort(a.begin(),a.end());
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]!=heights[i])
+            {
+                count++;
+            }
+        }
+        return count;
+        
+    }
+};
