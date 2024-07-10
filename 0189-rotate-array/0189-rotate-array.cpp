0e28c186ad3ed57efdb5f856ba70afdd72c0f985
Atharva Vijay Khairnar <atharva.khairnar_comp21@pccoer.in>
2024-07-10 15:02:01 +0530

    Time: 21 ms (55.6%), Space: 27.3 MB (56.55%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,22 +1,10 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-     int n=nums.size();
-     vector<int>a;
-     a=nums;
-     if(k%n==0)
-     {
-       k=0;     
-    }
-    else
-    {
+        int n=nums.size();
         k=k%n;
-    }
-   for(int i=0;i<k;i++)
-   {
-    nums.pop_back();
-   }
-     nums.insert(nums.begin(),a.begin()+(n-k),a.end());
-    
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
     }
 };
