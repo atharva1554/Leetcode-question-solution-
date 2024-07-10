059b7a8a86aad4d148e677df083aab572992300f
Atharva Vijay Khairnar <atharva.khairnar_comp21@pccoer.in>
2024-07-10 17:30:33 +0530

    Time: 4 ms (55.29%), Space: 14.7 MB (16.05%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -7,8 +7,8 @@ public:
         {
             if(nums[i]<nums[i+1])
             {
-              ind=i;
-              break;
+                ind=i;
+                break;
             }
         }
         if(ind==-1)
@@ -16,18 +16,15 @@ public:
             reverse(nums.begin(),nums.end());
             return;
         }
-        
-        
-            for(int i=n-1;i>ind;i--)
+        for(int i=n-1;i>ind;i--)
+        {
+            if(nums[i]>nums[ind])
             {
-                if(nums[i]>nums[ind])
-                {
-                      swap(nums[i],nums[ind]);
-                      break;
-                }
+                swap(nums[i],nums[ind]);
+                break;
             }
-            reverse(nums.begin()+ind+1,nums.end());
+        }
+        reverse(nums.begin()+ind+1,nums.end());
         
-    
     }
 };
