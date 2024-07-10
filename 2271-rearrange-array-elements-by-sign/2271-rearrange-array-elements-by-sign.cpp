7e997fdf9317d69684b52d4ffef84788d3dac368
Atharva Vijay Khairnar <atharva.khairnar_comp21@pccoer.in>
2024-07-10 16:35:42 +0530

    Time: 146 ms (47.76%), Space: 137 MB (32.68%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
@@ -3,25 +3,25 @@ public:
     vector<int> rearrangeArray(vector<int>& nums) {
         vector<int>p;
         vector<int>n;
-        int s=nums.size();
-        for(int i=0;i<s;i++)
+        for(int i=0;i<nums.size();i++)
         {
-            if(nums[i]<0)
-            {
-                n.push_back(nums[i]);
-            }
-            else
+            if(nums[i]>=0)
             {
                 p.push_back(nums[i]);
             }
-
+            else
+            {
+                n.push_back(nums[i]);
+            }
         }
+        
         nums.clear();
-        for(int i=0;i<p.size();i++)
-        {
-            nums.push_back(p[i]);
-            nums.push_back(n[i]);
-        }
+            for(int i=0;i<n.size();i++)
+            {
+                nums.push_back(p[i]);
+                nums.push_back(n[i]);
+            }
+        
         return nums;
     }
 };
