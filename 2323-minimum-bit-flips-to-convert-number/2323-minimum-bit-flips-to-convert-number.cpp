db2f20cf58c99fa81bf642f1ee51a996c1f23b60
Atharva Vijay Khairnar <atharva.khairnar_comp21@pccoer.in>
2024-07-17 16:18:43 +0530

    Time: 4 ms (43.65%), Space: 7.5 MB (6.23%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
-int sol(int n)
-{
-    int ans=0;
-    while(n>0)
-    {
-        if(n%2!=0)
-        {
-            ans++;
-        }
-        n=n/2;
-    }
-    return ans;
-}
+
     int minBitFlips(int start, int goal) {
         int ans=start^goal;
-        int d=sol(ans);
+        int d=0;
+        for(int i=0;i<=31;i++)
+        {
+            if(ans&(1<<i))
+            {
+                d++;
+            }
+        }
         return d;
+        
     }
 };
