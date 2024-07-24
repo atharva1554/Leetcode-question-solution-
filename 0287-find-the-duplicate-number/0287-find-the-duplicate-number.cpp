6f31341ed545ccd762d3d238d9dff994529d177e
Atharva Vijay Khairnar <atharva.khairnar_comp21@pccoer.in>
2024-07-24 16:39:05 +0530

    Time: 234 ms (5.06%), Space: 89.4 MB (9.62%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -2,16 +2,14 @@ class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
         map<int,int>mpp;
-        int a;
         for(int i=0;i<nums.size();i++)
         {
             mpp[nums[i]]++;
             if(mpp[nums[i]]==2)
             {
-                a= nums[i];
+                return nums[i];
             }
         }
-    
-        return a;
+        return 0;
     }
 };
