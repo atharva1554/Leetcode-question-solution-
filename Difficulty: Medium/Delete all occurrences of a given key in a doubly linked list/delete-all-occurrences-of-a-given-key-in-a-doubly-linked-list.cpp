9e0752fff059e2afd76e80b80a568d7514a78866
Atharva Vijay Khairnar <atharva.khairnar_comp21@pccoer.in>
2024-08-20 15:00:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Delete all occurrences of a given key in a doubly linked list/delete-all-occurrences-of-a-given-key-in-a-doubly-linked-list.cpp
@@ -0,0 +1,134 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* next;
+    struct Node* prev;
+};
+
+void push(struct Node** head_ref, int new_data) {
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+
+    new_node->data = new_data;
+
+    new_node->prev = NULL;
+
+    new_node->next = (*head_ref);
+
+    if ((*head_ref) != NULL) (*head_ref)->prev = new_node;
+
+    (*head_ref) = new_node;
+}
+
+void printList(struct Node* head) {
+    if (head == NULL) cout << -1;
+
+    while (head != NULL) {
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+
+/* a Node of the doubly linked list 
+struct Node
+{
+  int data;
+  struct Node *next;
+  struct Node *prev;
+  Node(int x) { data = x; next = prev = NULL; }
+}; */
+
+class Solution {
+public:
+    void deleteAllOccurOfX(struct Node** head_ref, int x) {
+        Node* temp = *head_ref;
+
+        // Traverse the list
+        while (temp != NULL) {
+            // If node to be deleted is found
+            if (temp->data == x) {
+                Node* nextNode = temp->next;
+
+                // If this node is the head node
+                if (temp->prev == NULL) {
+                    *head_ref = temp->next;
+                    if (*head_ref != NULL) {
+                        (*head_ref)->prev = NULL;
+                    }
+                }
+                // If this node is the last node
+                else if (temp->next == NULL) {
+                    temp->prev->next = NULL;
+                }
+                // If this node is in the middle
+                else {
+                    temp->prev->next = temp->next;
+                    temp->next->prev = temp->prev;
+                }
+
+                // Delete the current node
+                delete temp;
+
+                // Move to the next node
+                temp = nextNode;
+            } else {
+                // Move to the next node if no deletion occurred
+                temp = temp->next;
+            }
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t = 1;
+    cin >> t;
+
+    // freopen ("output_gfg.txt", "w", stdout);
+
+    while (t--) {
+        // Input
+        Node* head = new Node();
+        head = NULL;
+
+        int n;
+        cin >> n;
+
+        vector<int> inp(n);
+
+        for (int i = 0; i < n; i++) {
+            int val;
+            cin >> val;
+            // push(&head,val);
+            inp[i] = val;
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            push(&head, inp[i]);
+        }
+        int key;
+        cin >> key;
+        // printList(head);
+        // cout << endl;
+
+        Solution obj;
+
+        obj.deleteAllOccurOfX(&head, key);
+        printList(head);
+        cout << endl;
+
+        // cout << "\n~\n";
+    }
+    // fclose(stdout);
+
+    return 0;
+}
+// } Driver Code Ends
