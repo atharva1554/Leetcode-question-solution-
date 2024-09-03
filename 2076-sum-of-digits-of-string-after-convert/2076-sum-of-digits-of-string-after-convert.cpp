edd8dd4ce79b8e5edeefe2c879b4de2fa8517080
Atharva Vijay Khairnar <atharva.khairnar_comp21@pccoer.in>
2024-09-03 05:50:08 +0530

    Time: 4 ms (39.15%), Space: 11.4 MB (5.53%) - LeetHub

---
+++ 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int getLucky(string s, int k) 
+    {
+        string str="";
+        for(int i=0;i<s.length();i++)
+            str=str+ to_string(s[i]-'a'+1);
+        while(k!=0)
+        {
+            int ans=0;
+            for(int i=0;i<str.length();i++)
+                ans=ans+str[i]-'0';
+            str=to_string(ans);
+            k--;
+        }
+        return stoi(str);
+    }
+};
