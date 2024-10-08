ba9e2872aa403c6ebc6283cd89187f82019deecb
Atharva Vijay Khairnar <atharva.khairnar_comp21@pccoer.in>
2024-10-08 12:12:40 +0530

    Time: 96 ms (52.31%), Space: 70.2 MB (88.56%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
-    
-    int numberOfSubarrays(vector<int>& a, int k) {
-        int ans=0,sum=0,n=a.size();
-        map<int,int>mp;
-        mp[0]=1;
-        for(int i=0;i<n;i++){
-            sum+=a[i]%2; // converting even with 0 & odd wiht 1;
-            if(mp.find(sum-k)!=mp.end())
-                ans+=mp[sum-k];
-            mp[sum]++;
+int sol(vector<int>&num,int t)
+{
+    int n=num.size();
+    if(t<0)
+    {
+        return 0;
+    }
+    for(int i=0;i<n;i++)
+    {
+        num[i]=num[i]%2;
+    }
+    int l=0,r=0,cnt=0,sum=0;
+    while(r<n)
+    {
+        sum=sum+num[r];
+        while(sum>t)
+        {
+            sum=sum-num[l];
+            l=l+1;
         }
-        return ans;
+        cnt=cnt+(r-l+1);
+        r=r+1;
+    }
+    return cnt;
+}
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        return sol(nums,k)-sol(nums,k-1);
     }
 };
