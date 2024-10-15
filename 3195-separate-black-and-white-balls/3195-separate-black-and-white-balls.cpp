b480e55043f442ab6e8297fbdf69583ed7204b73
Atharva Vijay Khairnar <atharva.khairnar_comp21@pccoer.in>
2024-10-15 08:32:10 +0530

    Time: 31 ms (73.47%), Space: 15.6 MB (45.68%) - LeetHub

---
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    long long minimumSteps(string s) {
+        long long cnt=0,ans=0;
+        for(int i=s.size()-1;i>=0;i--)
+        {
+            if(s[i]=='1')
+            {
+                ans+=cnt;
+            }
+            else
+            {
+                cnt++;
+            }
+        }
+        return ans;
+
+    }
+};
