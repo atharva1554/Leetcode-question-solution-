48e100f8280b15ca41963ae7f801344c851b4579
Atharva Vijay Khairnar <atharva.khairnar_comp21@pccoer.in>
2024-11-11 12:35:01 +0530

    Time: 0 ms (100%), Space: 10.2 MB (51.15%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -11,56 +11,19 @@
  */
 class Solution {
 public:
+void postorder(TreeNode* node,vector<int>&ans)
+{
+    if(node==nullptr)
+    {
+        return;
+    }
+    postorder(node->left,ans);
+    postorder(node->right,ans);
+    ans.push_back(node->val);
+}
     vector<int> postorderTraversal(TreeNode* root) {
-        vector<int> nodes;
-        TreeNode* dummy = new TreeNode(0);
-        dummy -> left = root;
-        TreeNode* cur = dummy;
-        while (cur) {
-            if (cur -> left) {
-                TreeNode* pre = cur -> left;
-                while (pre -> right && (pre -> right != cur)) {
-                    pre = pre -> right;
-                }
-                if (!(pre -> right)) {
-                    pre -> right = cur;
-                    cur = cur -> left;
-                } else {
-                    reverseAddNodes(cur -> left, pre, nodes);
-                    pre -> right = NULL;
-                    cur = cur -> right;
-                }
-            } else {
-                cur = cur -> right;
-            }
-        }
-        return nodes;
-    }
-private:
-    void reverseNodes(TreeNode* start, TreeNode* end) {
-        if (start == end) {
-            return;
-        }
-        TreeNode* x = start;
-        TreeNode* y = start -> right;
-        TreeNode* z;
-        while (x != end) {
-            z = y -> right;
-            y -> right = x;
-            x = y;
-            y = z;
-        }
-    }
-    void reverseAddNodes(TreeNode* start, TreeNode* end, vector<int>& nodes) {
-        reverseNodes(start, end);
-        TreeNode* node = end;
-        while (true) {
-            nodes.push_back(node -> val);
-            if (node == start) {
-                break;
-            }
-            node = node -> right;
-        }
-        reverseNodes(end, start);
+        vector<int>ans;
+        postorder(root,ans);
+        return ans;
     }
 };
