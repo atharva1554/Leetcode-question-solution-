c56e1f9ead0a0a2a0f7d85ac31f9e0b267a4a9a7
Atharva Vijay Khairnar <atharva.khairnar_comp21@pccoer.in>
2025-01-02 15:38:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Move Last Element to Front of a Linked List/move-last-element-to-front-of-a-linked-list.cpp
@@ -0,0 +1,80 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node* head) {
+    Node* temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    Node *moveToFront(Node *head) {
+        // code here
+        Node *temp=head;
+        while(temp->next->next!=NULL)
+        {
+            temp=temp->next;
+        }
+        temp->next->next=head;
+        head=temp->next;
+        temp->next=NULL;
+        return head;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        if (arr.empty()) {
+            cout << -1 << endl;
+            continue;
+        }
+        Solution ob;
+        int data = arr[0];
+        Node* head = new Node(data);
+        Node* tail = head;
+        for (int i = 1; i < arr.size(); ++i) {
+            data = arr[i];
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+        head = ob.moveToFront(head);
+        printList(head);
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
