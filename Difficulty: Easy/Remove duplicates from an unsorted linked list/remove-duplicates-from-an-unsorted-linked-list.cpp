bebc1f54625c1f3fe5dea44459d53c5156cb8251
Atharva Vijay Khairnar <atharva.khairnar_comp21@pccoer.in>
2025-01-02 16:10:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove duplicates from an unsorted linked list/remove-duplicates-from-an-unsorted-linked-list.cpp
@@ -0,0 +1,104 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *next;
+
+    Node(int x)
+        : data(x)
+        , next(nullptr) {}
+};
+
+void printList(Node *head) {
+    Node *temp = head;
+    while (temp != nullptr) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+
+// } Driver Code Ends
+/*
+The structure of linked list is the following
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+#include<bits/stdc++.h>
+class Solution {
+  public:
+    Node *removeDuplicates(Node *head) {
+        // your code goes here
+        map<int,int>mpp;
+        Node *temp=head;
+        Node *t=head;
+        mpp[head->data]++;
+        Node *d=t;
+        
+        while(temp!=NULL)
+        {
+            if(mpp[temp->data]==0)
+            {
+                Node *dummy=new Node(temp->data);
+                t->next=dummy;
+                t=t->next;
+               mpp[temp->data]++;
+            }
+            temp=temp->next;
+            
+        }
+        return d;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        if (arr.empty()) {
+            cout << "empty" << endl;
+            continue;
+        }
+
+        Node *head = new Node(arr[0]);
+        Node *tail = head;
+
+        for (size_t i = 1; i < arr.size(); ++i) {
+            tail->next = new Node(arr[i]);
+            tail = tail->next;
+        }
+
+        Solution ob;
+        Node *result = ob.removeDuplicates(head);
+        printList(result);
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
