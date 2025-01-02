a4f1d3920387e25e9f250700686896b09b07e10a
Atharva Vijay Khairnar <atharva.khairnar_comp21@pccoer.in>
2025-01-02 18:04:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/XOR Linked List/xor-linked-list.cpp
@@ -0,0 +1,107 @@
+//{ Driver Code Starts
+#include <stdint.h>
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *npx;
+
+    Node(int x) {
+        data = x;
+        npx = NULL;
+    }
+};
+
+struct Node *XOR(struct Node *a, struct Node *b) {
+    return (struct Node *)((uintptr_t)(a) ^ (uintptr_t)(b));
+}
+
+struct Node *insert(struct Node *head, int data);
+
+vector<int> getList(struct Node *head);
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        struct Node *head = NULL;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+
+        while (ss >> number) {
+            head = insert(head, number);
+        }
+
+        vector<int> vec = getList(head);
+        for (int x : vec)
+            cout << x << " ";
+        cout << endl;
+        for (int i = vec.size() - 1; i >= 0; i--) {
+            cout << vec[i] << " ";
+        }
+        cout << "\n";
+        cout << "~" << endl;
+    }
+    return (0);
+}
+
+// } Driver Code Ends
+
+
+/*
+Structure of linked list is as
+struct Node
+{
+    int data;
+    struct Node* npx;
+
+    Node(int x){
+        data = x;
+        npx = NULL;
+    }
+};
+
+Utility function to get XOR of two Struct Node pointer
+use this function to get XOR of two pointers
+struct Node* XOR (struct Node *a, struct Node *b)
+{
+    return (struct Node*) ((uintptr_t) (a) ^ (uintptr_t) (b));
+}
+*/
+
+// function should insert the data to the front of the list
+struct Node *insert(struct Node *head, int data) {
+    // Code here
+    struct Node * newNode = new Node(data);
+    newNode->npx = head;
+ 
+    if(head!=NULL)
+        head->npx = XOR(head->npx,newNode);
+ 
+    return newNode;
+    
+}
+
+vector<int> getList(struct Node *head) {
+    // Code here
+    vector<int> list;
+    struct Node * curr = head;
+    struct Node * prev = NULL;
+    struct Node * next;
+
+    while(curr != NULL){
+
+        list.push_back(curr->data);
+        next = XOR(curr->npx, prev);
+        prev = curr;
+        curr = next;
+        
+    }
+
+    return list;
+}
