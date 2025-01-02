4eafe00c338ed3392abebb9509a642a0c6e758d6
Atharva Vijay Khairnar <atharva.khairnar_comp21@pccoer.in>
2025-01-02 17:18:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge Sort for Linked List/merge-sort-for-linked-list.cpp
@@ -0,0 +1,100 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node *node) {
+    while (node != NULL) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << "\n";
+}
+
+
+// } Driver Code Ends
+/*
+The structure of linked list is the following
+
+struct Node {
+    int data;
+    Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    // Function to sort the given linked list using Merge Sort.
+    Node* mergeSort(Node* head) {
+        // your code here
+        vector<int>a;
+        Node *temp=head;
+        while(temp)
+        {
+            a.push_back(temp->data);
+            temp=temp->next;
+        }
+        sort(a.begin(),a.end());
+        temp=head;
+        for(int i=0;i<a.size();i++)
+        {
+            temp->data=a[i];
+            temp=temp->next;
+        }
+        return head;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+
+        // Read numbers from the input line
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Node *head = nullptr;
+        // Check if the array is empty
+        if (!arr.empty()) {
+            head = new Node(arr[0]);
+            Node *tail = head;
+            for (size_t i = 1; i < arr.size(); ++i) {
+                tail->next = new Node(arr[i]);
+                tail = tail->next;
+            }
+        }
+        Solution obj;
+        Node *ans = obj.mergeSort(head);
+        printList(ans);
+        cout << "~\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
