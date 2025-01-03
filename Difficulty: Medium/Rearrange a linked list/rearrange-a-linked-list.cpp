b984dacc2320eff26ecaf0d7b63c9cb407202b54
Atharva Vijay Khairnar <atharva.khairnar_comp21@pccoer.in>
2025-01-03 19:18:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rearrange a linked list/rearrange-a-linked-list.cpp
@@ -0,0 +1,107 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node *node) {
+    while (node != NULL) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << endl
+         << "~"
+         << "\n";
+}
+
+
+// } Driver Code Ends
+/* Node structure
+
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    void rearrangeEvenOdd(Node *head) {
+        if (!head || !head->next) {
+            return; // If the list has 0 or 1 nodes, no rearrangement is needed
+        }
+
+        Node* odd = head;
+        Node* even = head->next;
+        Node* evenHead = even;
+
+        // Separate odd and even nodes
+        while (even && even->next) {
+            odd->next = even->next;
+            odd = odd->next;
+            even->next = odd->next;
+            even = even->next;
+        }
+        odd->next = NULL; // End the odd list
+
+        // Reverse the even list
+       
+
+        // Append the reversed even list to the end of the odd list
+        odd->next = evenHead;
+    }
+    
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+
+        // Read numbers from the input line
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Node *head = nullptr;
+        // Check if the array is empty
+        if (!arr.empty()) {
+            head = new Node(arr[0]);
+            Node *tail = head;
+            for (size_t i = 1; i < arr.size(); ++i) {
+                tail->next = new Node(arr[i]);
+                tail = tail->next;
+            }
+        }
+        Solution ob;
+        ob.rearrangeEvenOdd(head);
+        printList(head);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
