534f539ce8dcf13dea6f7be86f67bafd2564e937
Atharva Vijay Khairnar <atharva.khairnar_comp21@pccoer.in>
2025-01-04 19:04:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Clone List with Next and Random/clone-list-with-next-and-random.cpp
@@ -0,0 +1,181 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+#include <iostream>
+#include <map>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+/* Link list Node */
+struct Node {
+    int data;
+    Node *next;
+    Node *random;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+        random = NULL;
+    }
+};
+
+void print(Node *root) {
+    Node *temp = root;
+    while (temp != NULL) {
+        int k;
+        if (temp->random == NULL)
+            k = -1;
+        else
+            k = temp->random->data;
+        cout << temp->data << " " << k << " ";
+        temp = temp->next;
+    }
+}
+
+void append(Node **head_ref, Node **tail_ref, int new_data) {
+    Node *new_node = new Node(new_data);
+    if (*head_ref == NULL) {
+        *head_ref = new_node;
+    } else {
+        (*tail_ref)->next = new_node;
+    }
+    *tail_ref = new_node;
+}
+
+bool validation(Node *head, Node *res) {
+    Node *temp1 = head;
+    Node *temp2 = res;
+
+    while (temp1 != NULL && temp2 != NULL) {
+        if (temp1->data != temp2->data)
+            return false;
+        if ((temp1->random == NULL && temp2->random != NULL) ||
+            (temp1->random != NULL && temp2->random == NULL) ||
+            (temp1->random != NULL && temp2->random != NULL &&
+             temp1->random->data != temp2->random->data))
+            return false;
+        temp1 = temp1->next;
+        temp2 = temp2->next;
+    }
+    return true;
+}
+
+
+// } Driver Code Ends
+/* Link list Node
+struct Node {
+    int data;
+    Node *next;
+    Node *random;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+        random = NULL;
+    }
+};*/
+
+class Solution {
+public:
+    Node* cloneLinkedList(Node* head) {
+        if (head == NULL) return NULL;
+
+        // Step 1: Create new nodes and insert them between the original nodes.
+        Node* current = head;
+        while (current != NULL) {
+            Node* newNode = new Node(current->data);
+            newNode->next = current->next;
+            current->next = newNode;
+            current = newNode->next;  // Move to the next original node.
+        }
+
+        // Step 2: Copy the random pointers of the new nodes.
+        current = head;
+        while (current != NULL) {
+            if (current->random != NULL) {
+                current->next->random = current->random->next;
+            }
+            current = current->next->next;  // Move to the next original node.
+        }
+
+        // Step 3: Separate the cloned list from the original list.
+        Node* newHead = head->next;
+        Node* original = head;
+        Node* copy = newHead;
+        while (original != NULL) {
+            original->next = original->next->next;  // Restore original list.
+            if (copy->next != NULL) {
+                copy->next = copy->next->next;  // Set the next of copied node.
+            }
+            original = original->next;
+            copy = copy->next;
+        }
+
+        return newHead;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int T;
+    cin >> T;
+    cin.ignore(); // Ignore the newline after T
+    while (T--) {
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        vector<int> arr;
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Node *head = NULL, *tail = NULL;
+        for (int i = 0; i < arr.size(); ++i) {
+            append(&head, &tail, arr[i]);
+        }
+
+        getline(cin, input);
+        stringstream ss2(input);
+        vector<int> arr2;
+        while (ss2 >> number) {
+            arr2.push_back(number);
+        }
+
+        Node *temp = head;
+        for (int i = 0; i < arr2.size(); i += 2) {
+            int a = arr2[i];
+            int b = arr2[i + 1];
+
+            Node *tempA = head;
+            Node *tempB = head;
+            for (int j = 1; j < a; ++j) {
+                tempA = tempA->next;
+            }
+            for (int j = 1; j < b; ++j) {
+                tempB = tempB->next;
+            }
+
+            tempA->random = tempB;
+        }
+
+        Solution ob;
+        Node *res = ob.cloneLinkedList(head);
+        if (!res or res == head) {
+            cout << "false" << endl;
+            continue;
+        }
+        if (validation(head, res)) {
+            cout << "true" << endl;
+        } else {
+            cout << "false" << endl;
+        }
+        cout << "~\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
