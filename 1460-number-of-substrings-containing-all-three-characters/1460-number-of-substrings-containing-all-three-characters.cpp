3dbbb2c75bd822fdfe08cd7698359c1d2e28a6f6
Atharva Vijay Khairnar <atharva.khairnar_comp21@pccoer.in>
2025-03-11 06:22:02 +0530

    Time: 44 ms (7.88%), Space: 16.8 MB (12.26%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        int cnt=0;
-        int last[3]={-1};
-        int n=s.size();
-         int mini=0;
-        for(int i=0;i<n;i++)
-        {
-            last[s[i]-'a']=i;
-            if(last[0]!=-1 && last[1]!=-1 && last[2]!=-1)
-            {
-              mini =min(last[0],mini);
-               mini =min(last[1],mini);
-                mini =min(last[2],mini);
-                cnt=cnt+mini+1;
+        unordered_map<char, int> freq;
+        int left = 0, cnt = 0;
+        for (int i = 0; i < s.size(); i++) {
+            freq[s[i]]++;
+            while (freq.size() == 3) {
+                cnt += s.size() - i;
+                freq[s[left]]--;
+                if (freq[s[left]] == 0) freq.erase(s[left]);
+                left++;
             }
         }
         return cnt;
