e4c5c8a0b158fe88403150bc0e7b224cab024d75
Atharva Vijay Khairnar <atharva.khairnar_comp21@pccoer.in>
2025-03-24 11:37:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int matrixMultiplication(vector<int> &arr) {
+        int n = arr.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        for (int len = 2; len < n; len++) {
+            for (int i = 0; i < n - len; i++) {
+                int j = i + len;
+                dp[i][j] = INT_MAX;
+                for (int k = i + 1; k < j; k++) {
+                    int cost = dp[i][k] + dp[k][j] + arr[i] * arr[k] * arr[j];
+                    dp[i][j] = min(dp[i][j], cost);
+                }
+            }
+        }
+        return dp[0][n - 1];
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // to ignore the newline after the integer input
+    while (t--) {
+        int n;
+        vector<int> a;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num);
+
+        Solution obj;
+        cout << obj.matrixMultiplication(a) << endl << "~\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
