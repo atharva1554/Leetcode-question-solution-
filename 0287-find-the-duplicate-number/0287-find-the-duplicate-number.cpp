1e77943c024515462339750635cb4ad7bd48df99
Atharva Vijay Khairnar <atharva.khairnar_comp21@pccoer.in>
2025-04-24 18:07:42 +0530

    Time: 156 ms (5.04%), Space: 89.5 MB (8.48%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -5,7 +5,7 @@ public:
         for(int i=0;i<nums.size();i++)
         {
             mpp[nums[i]]++;
-            if(mpp[nums[i]]==2)
+            if( mpp[nums[i]]++>1)
             {
                 return nums[i];
             }
