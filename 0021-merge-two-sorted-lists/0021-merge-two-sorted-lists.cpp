d49795bc5995bacad6dee49137954b2ed72160b1
Atharva Vijay Khairnar <atharva.khairnar_comp21@pccoer.in>
2025-04-28 09:18:58 +0530

    Time: 0 ms (100%), Space: 19.5 MB (62.1%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,37 +11,24 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode*t1=list1;
-        ListNode*t2=list2;
-        ListNode*dummyhead=new ListNode(-1);
-        ListNode*temp=dummyhead;
-        
-        while((t1!=nullptr)&&(t2!=nullptr))
-        {
-            if((t1->val)<(t2->val))
-            {
-                temp->next=t1;
-                temp=temp->next;
-                t1=t1->next;
+        ListNode dummy(0); // Create a dummy node
+        ListNode* temp = &dummy; // temp points to dummy
+
+        while (list1 != nullptr && list2 != nullptr) {
+            if (list1->val < list2->val) {
+                temp->next = list1;
+                list1 = list1->next;
+            } else {
+                temp->next = list2;
+                list2 = list2->next;
             }
-            else
-            {
-                temp->next=t2;
-                temp=temp->next;
-                t2=t2->next;
-            }
+            temp = temp->next;
         }
-        while(t1!=nullptr)
-        {   temp->next=t1;
-            t1=t1->next;
-            temp=temp->next;
-        }
-        while(t2!=nullptr)
-        {
-            temp->next=t2;
-            t2=t2->next;
-            temp=temp->next;
-        }
-        return dummyhead->next;
+
+        // Attach the remaining part
+        if (list1 != nullptr) temp->next = list1;
+        else temp->next = list2;
+
+        return dummy.next; // The real head is next of dummy
     }
 };
