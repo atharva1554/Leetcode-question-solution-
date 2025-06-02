f1f5436feac35d689a1e24ae912fb46b59168abf
Atharva Vijay Khairnar <atharva.khairnar_comp21@pccoer.in>
2025-06-02 11:18:05 +0530

    Time: 0 ms (100%), Space: 22.9 MB (59.49%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,38 +1,18 @@
 class Solution {
 public:
-    int candy(vector<int>& ratings) {
-        vector<int>l;
-        vector<int>r;
-        int ans=0;
-        l.push_back(1);
-        r.push_back(1);
-        for(int i=1;i<ratings.size();i++)
-        {
-            if(ratings[i]>ratings[i-1])
-            {
-                l.push_back(l.back()+1);
-            }
-            else
-            {
-                l.push_back(1);
-            }
+    int candy(vector<int>& ratings, int cnt = 0) {
+        int n = ratings.size();
+        vector<int> candies(n, 1);
+
+        for (int i = 1; i < n; i++) 
+            if (ratings[i] > ratings[i - 1])
+                candies[i] = candies[i - 1] + 1;
+        
+        for (int i = n - 1; i > 0; i--) {
+            if (ratings[i - 1] > ratings[i])
+                candies[i - 1] = max(candies[i] + 1, candies[i - 1]);
+            cnt += candies[i - 1];
         }
-        for(int i=ratings.size()-2;i>=0;i--)
-        {
-            if(ratings[i]>ratings[i+1])
-            {
-                r.push_back(r.back()+1);
-            }
-            else
-            {
-                r.push_back(1);
-            }
-        }
-        reverse(l.begin(),l.end());
-        for(int i=0;i<l.size();i++)
-        {
-            ans=ans+max(r[i],l[i]);
-        }
-        return ans;
+        return cnt + candies[n - 1];
     }
 };
