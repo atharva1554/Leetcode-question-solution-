9e67533472b0defea361b8b10aad56982580b746
Atharva Vijay Khairnar <atharva.khairnar_comp21@pccoer.in>
2025-06-10 07:49:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Exactly one swap/exactly-one-swap.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    int countStrings(string &s) {
+
+        int arr[256] ={0};
+
+        int count = 0;
+
+        bool flag = false;
+
+        for(int i =0;i<s.size();i++){
+
+            count += s.size()-i-1;
+
+            count -= arr[s[i]];
+
+            if(arr[s[i]] != 0)flag = true;
+
+            arr[s[i]]++;
+
+        }
+
+        return count+flag;
+
+    }
+};
