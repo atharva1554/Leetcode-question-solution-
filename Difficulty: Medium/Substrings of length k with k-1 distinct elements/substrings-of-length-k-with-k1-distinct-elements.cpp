0439fcc596c4784f422e456b3c3b19bc02d20be2
Atharva Vijay Khairnar <atharva.khairnar_comp21@pccoer.in>
2025-07-01 21:52:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Substrings of length k with k-1 distinct elements/substrings-of-length-k-with-k1-distinct-elements.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+   int substrCount(string &s, int k) {
+        unordered_map<char, int>mp;
+        int ans=0;
+        for(int i=0; i<k; i++){mp[s[i]]++;}
+        if(mp.size()==k-1){ans++;}
+        for(int i=k, j=0;i<s.size(); i++, j++){
+           if(mp[s[j]]==1){mp.erase(s[j]);}
+           else{mp[s[j]]--;}
+           mp[s[i]]++;
+           if(mp.size()==k-1){ans++;}
+        }
+        return ans;
+    }
+};
